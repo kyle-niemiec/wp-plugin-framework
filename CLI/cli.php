8a6041b61ed3ff59d77ddd13156fcb5c73ad8c9d
Kyle Niemiec <kyle@codeflower.io>
2026-01-18 15:24:48 -0700

    Downgraded Symfony Console and installed Doctum.

--- CLI/cli.php
+++ CLI/cli.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Application;
 $application = new Application( 'WP Plugin Framework CLI', '0.0.1' );
 
 // Register commands
-$application->addCommand( new CreatePluginCommand );
+$application->add( new CreatePluginCommand );
 
 // Run the CLI
 $application->run();
