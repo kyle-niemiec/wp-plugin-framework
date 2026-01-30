6ec4306ce05d482de000e7d42d55a88c30acd871
Kyle Niemiec <kyle@codeflower.io>
2026-01-30 01:58:35 -0700

    Added last conditional question.

--- CLI/Command/CreatePostTypeCommand.php
+++ CLI/Command/CreatePostTypeCommand.php
@@ -14,12 +14,14 @@
 namespace WPPF\CLI\Command;
 
 use WPPF\CLI\Static\HelperBundle;
+use WPPF\CLI\Static\StyleUtil;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use WPPF\CLI\Static\ConsoleColor;
 
 /**
  * A command to create a post type class for the current plugin.
@@ -45,6 +47,25 @@ final class CreatePostTypeCommand extends Command
 		$singular_name = self::ask_singular_name( $bundle );
 		$plural_name = self::ask_plural_name( $bundle );
 		$show_in_menu = self::ask_show_in_menu( $bundle );
+		$menu_name = $singular_name;
+
+		if ( $show_in_menu ) {
+			$menu_name = self::ask_menu_entry_title( $bundle, $singular_name );
+		}
+
+		$output->writeln(
+			StyleUtil::color(
+				'Post type file was created!',
+				ConsoleColor::Green
+			)
+		);
+
+		$output->writeln(
+			StyleUtil::color(
+				'See \\WPPF\\v1_2_1\\WordPress\\Post_Type for the complete list of post type options.',
+				ConsoleColor::Gray
+			)
+		);
 
 		return Command::SUCCESS;
 	}
@@ -58,7 +79,7 @@ final class CreatePostTypeCommand extends Command
 	 */
 	private static function ask_singular_name( HelperBundle $bundle ): string
 	{
-		$question = new Question( 'Post type singular name: ' );
+		$question = new Question( 'What is the singular name of the custom post type? ' );
 
 		$question->setValidator( function ( $value ): string {
 			if ( null === $value || '' === trim( $value ) ) {
@@ -80,7 +101,7 @@ final class CreatePostTypeCommand extends Command
 	 */
 	private static function ask_plural_name( HelperBundle $bundle ): string
 	{
-		$question = new Question( 'Post type plural name: ' );
+		$question = new Question( 'What is the plural name of the custom post type? ' );
 
 		$question->setValidator( function ( $value ): string {
 			if ( null === $value || '' === trim( $value ) ) {
@@ -107,7 +128,7 @@ final class CreatePostTypeCommand extends Command
 		$question->setValidator( function ( $value ): string {
 			$value = strtolower( trim( strval( $value ) ) );
 
-			if ( ! in_array( $value, ['yes', 'no', 'y', 'n'] ) ) {
+			if ( ! in_array( $value, array( 'yes', 'no', 'y', 'n' ) ) ) {
 				throw new \RuntimeException( 'Please answer yes or no.' );
 			}
 
@@ -115,6 +136,37 @@ final class CreatePostTypeCommand extends Command
 		} );
 
 		$answer = $bundle->helper->ask( $bundle->input, $bundle->output, $question );
-		return in_array( $answer, ['yes', 'y'] );
+		return in_array( $answer, array( 'yes', 'y' ) );
+	}
+
+	/**
+	 * Ask the user for the admin menu entry title.
+	 *
+	 * @param HelperBundle $bundle The bundle containing the question and IO interfaces.
+	 * @param string $singular_name The singular name for the default.
+	 *
+	 * @return string The menu entry title entered by the user.
+	 */
+	private static function ask_menu_entry_title( HelperBundle $bundle, string $singular_name ): string
+	{
+		$question = new Question(
+			sprintf(
+				"What do you want the menu entry title to say? %s ",
+				StyleUtil::optional( sprintf( "Default: '%s'", $singular_name ) )
+			),
+			$singular_name
+		);
+
+		$question->setValidator( function ( $value ) use ( $singular_name ): string {
+			$clean = trim( strval( $value ) );
+
+			if ( '' === $clean ) {
+				return $singular_name;
+			}
+
+			return $clean;
+		} );
+
+		return $bundle->helper->ask( $bundle->input, $bundle->output, $question );
 	}
 }
